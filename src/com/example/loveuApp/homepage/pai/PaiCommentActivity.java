2f06fdf39118cbbdbc3722c1c56c21e64b48d758
baiguoqing <1215356195@qq.com>
2016-08-02 18:56:13 +0800

    pai完成

---
+++ src/com/example/loveuApp/homepage/pai/PaiCommentActivity.java
@@ -0,0 +1,15 @@
+package com.example.loveuApp.homepage.pai;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by dy on 2016/8/2.
+ */
+public class PaiCommentActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
