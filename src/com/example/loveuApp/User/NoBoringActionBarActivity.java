b33711ef94d4391d0c01c3fd9228c987f79421a7
Liuxin0 <liuxin@imudges.com>
2016-08-02 17:40:51 +0800

    dimens

---
+++ src/com/example/loveuApp/User/NoBoringActionBarActivity.java
@@ -0,0 +1,179 @@
+package com.example.loveuApp.User;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.graphics.RectF;
+import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.AbsListView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import com.example.loveuApp.R;
+
+import java.util.ArrayList;
+
+public class NoBoringActionBarActivity extends Activity {
+
+    private static final String TAG = "NoBoringActionBarActivity";
+    private int mActionBarTitleColor;
+    private int mActionBarHeight;
+    private int mHeaderHeight;
+    private int mMinHeaderTranslation;
+    private ListView mListView;
+    private KenBurnsView mHeaderPicture;
+    private ImageView mHeaderLogo;
+    private View mHeader;
+    private View mPlaceHolderView;
+    private AccelerateDecelerateInterpolator mSmoothInterpolator;
+
+    private RectF mRect1 = new RectF();
+    private RectF mRect2 = new RectF();
+
+    private AlphaForegroundColorSpan mAlphaForegroundColorSpan;
+    private SpannableString mSpannableString;
+
+    private TypedValue mTypedValue = new TypedValue();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mSmoothInterpolator = new AccelerateDecelerateInterpolator();
+        mHeaderHeight = getResources().getDimensionPixelSize(R.dimen.header_height);
+        mMinHeaderTranslation = -mHeaderHeight + getActionBarHeight();
+
+        setContentView(R.layout.activity_noboringactionbar);
+
+        mListView = (ListView) findViewById(R.id.listview);
+        mHeader = findViewById(R.id.header);
+        mHeaderPicture = (KenBurnsView) findViewById(R.id.header_picture);
+        mHeaderPicture.setResourceIds(R.drawable.picture0, R.drawable.picture1);
+        mHeaderLogo = (ImageView) findViewById(R.id.header_logo);
+
+        mActionBarTitleColor = getResources().getColor(R.color.actionbar_title_color);
+
+        mSpannableString = new SpannableString(getString(R.string.noboringactionbar_title));
+        mAlphaForegroundColorSpan = new AlphaForegroundColorSpan(mActionBarTitleColor);
+
+        setupActionBar();
+        setupListView();
+    }
+
+    private void setupListView() {
+        ArrayList<String> FAKES = new ArrayList<String>();
+        FAKES.add(0, "昵称");
+        FAKES.add(1, "签名");
+        FAKES.add(2, "性别");
+        FAKES.add(3, "年龄");
+        FAKES.add(4, "");
+        FAKES.add(5, "所在地");
+        FAKES.add(6, "故乡");
+        FAKES.add(7, "兴趣爱好");
+        FAKES.add(8, "修改密码");
+        FAKES.add(9, "退出登录");
+
+        mPlaceHolderView = getLayoutInflater().inflate(R.layout.view_header_placeholder, mListView, false);
+        mListView.addHeaderView(mPlaceHolderView);
+        mListView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, FAKES));
+        mListView.setMinimumHeight(20);
+//        移动题头
+        mListView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                int scrollY = getScrollY();
+                //sticky actionbar
+                mHeader.setTranslationY(Math.max(-scrollY, mMinHeaderTranslation));
+                //header_logo --> actionbar icon
+                float ratio = clamp(mHeader.getTranslationY() / mMinHeaderTranslation, 0.0f, 1.0f);
+                interpolate(mHeaderLogo, getActionBarIconView(), mSmoothInterpolator.getInterpolation(ratio));
+                //actionbar title alpha
+                //getActionBarTitleView().setAlpha(clamp(5.0F * ratio - 4.0F, 0.0F, 1.0F));
+                //---------------------------------
+                //better way thanks to @cyrilmottier
+                setTitleAlpha(clamp(5.0F * ratio - 4.0F, 0.0F, 1.0F));
+            }
+        });
+    }
+
+    private void setTitleAlpha(float alpha) {
+        mAlphaForegroundColorSpan.setAlpha(alpha);
+        mSpannableString.setSpan(mAlphaForegroundColorSpan, 0, mSpannableString.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        getActionBar().setTitle(mSpannableString);
+    }
+
+    public static float clamp(float value, float min, float max) {
+        return Math.max(min, Math.min(value, max));
+    }
+
+    private void interpolate(View view1, View view2, float interpolation) {
+        getOnScreenRect(mRect1, view1);
+        getOnScreenRect(mRect2, view2);
+
+        float scaleX = 1.0F + interpolation * (mRect2.width() / mRect1.width() - 1.0F);
+        float scaleY = 1.0F + interpolation * (mRect2.height() / mRect1.height() - 1.0F);
+        float translationX = 0.5F * (interpolation * (mRect2.left + mRect2.right - mRect1.left - mRect1.right));
+        float translationY = 0.5F * (interpolation * (mRect2.top + mRect2.bottom - mRect1.top - mRect1.bottom));
+
+        view1.setTranslationX(translationX);
+        view1.setTranslationY(translationY - mHeader.getTranslationY());
+        view1.setScaleX(scaleX);
+        view1.setScaleY(scaleY);
+    }
+
+    private RectF getOnScreenRect(RectF rect, View view) {
+        rect.set(view.getLeft(), view.getTop(), view.getRight(), view.getBottom());
+        return rect;
+    }
+
+    //    获取scroll位置
+    public int getScrollY() {
+        View c = mListView.getChildAt(0);
+        if (c == null) {
+            return 0;
+        }
+
+        int firstVisiblePosition = mListView.getFirstVisiblePosition();
+        int top = c.getTop();
+
+        int headerHeight = 0;
+        if (firstVisiblePosition >= 1) {
+            headerHeight = mPlaceHolderView.getHeight();
+        }
+
+        return -top + firstVisiblePosition * c.getHeight() + headerHeight;
+    }
+
+    private void setupActionBar() {
+        ActionBar actionBar = getActionBar();
+        actionBar.setIcon(R.drawable.ic_transparent);
+
+        //getActionBarTitleView().setAlpha(0f);
+    }
+
+    private ImageView getActionBarIconView() {
+        return (ImageView) findViewById(android.R.id.home);
+    }
+
+    /*private TextView getActionBarTitleView() {
+        int id = Resources.getSystem().getIdentifier("action_bar_title", "id", "android");
+        return (TextView) findViewById(id);
+    }*/
+
+    public int getActionBarHeight() {
+        if (mActionBarHeight != 0) {
+            return mActionBarHeight;
+        }
+        getTheme().resolveAttribute(android.R.attr.actionBarSize, mTypedValue, true);
+        mActionBarHeight = TypedValue.complexToDimensionPixelSize(mTypedValue.data, getResources().getDisplayMetrics());
+        return mActionBarHeight;
+    }
+}
--- src/com/example/loveuApp/message/MessageFragment.java
+++ src/com/example/loveuApp/message/MessageFragment.java
@@ -1,18 +1,31 @@
 package com.example.loveuApp.message;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import com.example.loveuApp.R;
+import com.example.loveuApp.User.NoBoringActionBarActivity;
 
 
 public class MessageFragment extends Fragment{
+
+    private Button btn;
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.messagemain, container, false);
+        btn = (Button) view.findViewById(R.id.to_user);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(),NoBoringActionBarActivity.class);
+                startActivity(intent);
+            }
+        });
         return view;
     }
 
